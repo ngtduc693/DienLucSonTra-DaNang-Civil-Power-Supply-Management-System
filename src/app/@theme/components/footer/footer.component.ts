cf2a97fe0b50b57786073a551cdfae223c5967c8
thanhduc93nguyen <thanhduc93.nguyen@gmail.com>
2020-09-13 11:57:48 +0700

    Footer amend

--- src/app/@theme/components/footer/footer.component.ts
+++ src/app/@theme/components/footer/footer.component.ts
@@ -5,11 +5,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.scss'],
   template: `
     <span class="created-by">
-      Tạo bởi ♥ <b><a href="tel:0826540246" target="_blank">Nguyễn Thành Đức</a></b> 2020
+      Tạo bởi ♥ <b><a href="tel:0966999907" target="_blank">Điện lực Sơn Trà - Phiên bản: 2020-09-08 </a></b>
     </span>
     <div class="socials">
       <a href="#" target="_blank" class="ion ion-social-github"></a>
-      <a href="https://www.facebook.com/thanhduc93.nguyen" target="_blank" class="ion ion-social-facebook"></a>
+      <a href="https://www.facebook.com/evncpc.vn" target="_blank" class="ion ion-social-facebook"></a>
       <a href="#" target="_blank" class="ion ion-social-twitter"></a>
       <a href="#" target="_blank" class="ion ion-social-linkedin"></a>
     </div>
